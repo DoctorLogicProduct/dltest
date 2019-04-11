af19ac7a87177ac0877827eb97ce944a967b39e4
Matthew Sparks <msparks@doctorlogic.com>
2019-04-11 21:17:30 +0000

    Deploy from Forestry.io

--- js/content-multiplier.js
+++ js/content-multiplier.js
@@ -1,52 +1,24 @@
-if ($(window).width() >= 568) {
 tl = new TimelineMax({ onUpdate: onUpdate });
-    const controller = new ScrollMagic.Controller();
+const controller = new ScrollMagic.Controller();
 
-tl.from('#title', 1, {opacity: 0});
-tl.from('#before', 1, {opacity: 0}, "1");
-tl.from('#after', 1, {opacity: 0}, "2");
-tl.from('#before-label', 1, {opacity: 0}, "3");
-tl.from('#after-label', 1, { opacity: 0 }, "4");
-tl.from('#description', 1, { opacity: 0 }, "5");
+tl.to('.image-group__image--1', 1, { rotationX: -65, rotationY: -40, rotationZ: -17, delay: 1.5 }), "3";
+tl.to('.image-group__image--2', 1, { opacity: 1 }), "4";
+tl.to('.image-group__image--3', 1, { opacity: 1 }), "5";
+tl.to('.image-group__image--4', 1, { opacity: 1 }), "6";
 
-    const scene = new ScrollMagic.Scene({
-        triggerElement: '.content-multiplier__image-group--1',
-        triggerHook: .5, //between 0 an 1
-        duration: '100%'
-    })
-        // .setPin('.image-group__container')
-        .setTween(tl)
-        // .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
-        .addTo(controller);
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.image-group__container',
+    offset: -200,
+    triggerHook: 0, //between 0 an 1
+    duration: '500%'
+})  
+    .setPin('.image-group__container', { pushFollowers: true })
+    .setTween(tl)
+    .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
+    .addTo(controller);
 
-    function onUpdate() {
-        //tl.progress();
-        //console.log('bob');
-    }
-} else {
-    tl = new TimelineMax({ onUpdate: onUpdate });
-    const controller = new ScrollMagic.Controller();
-
-    tl.from('#title', 1, { opacity: 0 });
-    tl.from('#before', 1, { opacity: 0 }, "1");
-    tl.from('#after', 1, { opacity: 0 }, "2");
-    tl.from('#before-label', 1, { opacity: 0 }, "3");
-    tl.from('#after-label', 1, { opacity: 0 }, "4");
-    tl.from('#description', 1, { opacity: 0 }, "5");
-
-    const scene = new ScrollMagic.Scene({
-        triggerElement: '.content-multiplier__image-group--1',
-        triggerHook: .5, //between 0 an 1
-        duration: '100%'
-    })
-        // .setPin('.image-group__container')
-        .setTween(tl)
-        // .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
-        .addTo(controller);
-
-    function onUpdate() {
-        //tl.progress();
-        //console.log('bob');
-    }
+function onUpdate() {
+    //tl.progress();
+    //console.log('bob');
 }
 
