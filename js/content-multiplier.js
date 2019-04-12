87cbd5383bd28b30c1173e9aa1e29734d660434e
Matthew Sparks <msparks@doctorlogic.com>
2019-04-12 18:53:55 +0000

    Deploy from Forestry.io

--- js/content-multiplier.js
+++ js/content-multiplier.js
@@ -1,24 +1,48 @@
+if ($(window).width() >= 990) {
 tl = new TimelineMax({ onUpdate: onUpdate });
 const controller = new ScrollMagic.Controller();
 
-tl.to('.image-group__image--1', 1, { rotationX: -65, rotationY: -40, rotationZ: -17, delay: 1.5 }), "3";
-tl.to('.image-group__image--2', 1, { opacity: 1 }), "4";
-tl.to('.image-group__image--3', 1, { opacity: 1 }), "5";
-tl.to('.image-group__image--4', 1, { opacity: 1 }), "6";
+tl.to('.item-grid__image--1', 1, { rotationX: -65, rotationY: -40, rotationZ: -17, delay: 1.5 }), "3";
+tl.to('.item-grid__item--2', 1, { opacity: 1 }), "4";
+tl.to('.item-grid__item--3', 1, { opacity: 1 }), "5";
+tl.to('.item-grid__item--4', 1, { opacity: 1 }), "6";
+tl.to('.btn', 1, {opacity: 1 }), "6";
 
 const scene = new ScrollMagic.Scene({
-    triggerElement: '.image-group__container',
-    offset: -200,
+    triggerElement: '.item-grid__items',
+    offset: -150,
     triggerHook: 0, //between 0 an 1
     duration: '500%'
 })  
-    .setPin('.image-group__container', { pushFollowers: true })
+    .setPin('.item-grid__items', { pushFollowers: true })
     .setTween(tl)
-    .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
+    // .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
     .addTo(controller);
 
 function onUpdate() {
     //tl.progress();
     //console.log('bob');
+}
+} else {
+    tl = new TimelineMax({ onUpdate: onUpdate });
+    const controller = new ScrollMagic.Controller();
+    tl.to('.item-grid__item--2', 1, { opacity: 1 });
+    tl.to('.item-grid__item--3', 1, { opacity: 1 });
+    tl.to('.item-grid__item--4', 1, { opacity: 1 });
+    tl.to('.btn', 1, { opacity: 1 });
+
+    const scene = new ScrollMagic.Scene({
+        triggerElement: '.item-grid__items',
+        triggerHook: .5, //between 0 an 1
+        duration: '100%'
+    })
+        .setTween(tl)
+        // .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
+        .addTo(controller);
+
+    function onUpdate() {
+        //tl.progress();
+        //console.log('bob');
+    }
 }
 
