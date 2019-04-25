b9799ddb4887c56ad664a575c1a1c2c2cf57079d
Matthew Sparks <msparks@doctorlogic.com>
2019-04-25 19:27:27 +0000

    Deploy from Forestry.io

--- js/content-multiplier.js
+++ js/content-multiplier.js
@@ -2,19 +2,19 @@ if ($(window).width() >= 990) {
 tl = new TimelineMax({ onUpdate: onUpdate });
 const controller = new ScrollMagic.Controller();
 
-tl.to('.item-grid__image--1', 1, { rotationX: -65, rotationY: -40, rotationZ: -17, delay: 1.5 }), "3";
+tl.to('.item-grid__image--1', 1, { rotationX: -65, rotationY: -40, rotationZ: -17, delay: 1.5 }), "1";
 tl.to('.item-grid__item--2', 1, { opacity: 1 }), "4";
 tl.to('.item-grid__item--3', 1, { opacity: 1 }), "5";
 tl.to('.item-grid__item--4', 1, { opacity: 1 }), "6";
 tl.to('.btn', 1, {opacity: 1 }), "6";
 
 const scene = new ScrollMagic.Scene({
-    triggerElement: '.item-grid__items',
-    offset: -150,
+    triggerElement: '.item-grid__container',
+    offset: 0,
     triggerHook: 0, //between 0 an 1
     duration: '500%'
 })  
-    .setPin('.item-grid__items', { pushFollowers: true })
+    .setPin('.item-grid__container', { pushFollowers: true })
     .setTween(tl)
     // .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
     .addTo(controller);
