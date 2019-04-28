37aa591bbd3bd1ad67c94d30026a3e23b21ad8cf
Matthew Sparks <msparks@doctorlogic.com>
2019-04-28 20:58:44 +0000

    Deploy from Forestry.io

--- js/growth-accelerators.js
+++ js/growth-accelerators.js
@@ -1,3 +1,4 @@
+if ($(window).width() >= 568) {
 tl = new TimelineMax({ onUpdate: onUpdate });
     const controller = new ScrollMagic.Controller();
 
@@ -28,4 +29,28 @@ tl.to('.image-group__image--6', 2, { y: -250, opacity: 1 }, '-=2');
         //tl.progress();
         //console.log('bob');
     }
+} else {
+    tl = new TimelineMax({ onUpdate: onUpdate });
+    const controller = new ScrollMagic.Controller();
 
+    tl.to('.image-group__image--1', 2, { y: -250, opacity: 1 });
+    tl.to('.image-group__image--1', 2, { delay: 6, y: -400, opacity: 0 });
+    tl.to('.image-group__image--2', 2, { y: -250, opacity: 1 }, '-=2');
+    tl.to('.image-group__image--2', 2, { delay: 6, y: -400, opacity: 0 });
+    tl.to('.image-group__image--3', 2, { y: -250, opacity: 1 }, '-=2');
+    const scene = new ScrollMagic.Scene({
+        triggerElement: '.growth-accelerators__image-group--1',
+        offset: -100,
+        triggerHook: .7, //between 0 an 1
+        duration: '100%'
+    })
+        // .setPin('.growth-accelerators__image-group--1', { pushFollowers: true })
+        .setTween(tl)
+        // .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
+        .addTo(controller);
+
+    function onUpdate() {
+        //tl.progress();
+        //console.log('bob');
+    }
+}
