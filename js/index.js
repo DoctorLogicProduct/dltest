c360b5a8e40b867e4f6d79e2a5a2346d4c5af2e0
Matthew Sparks <msparks@doctorlogic.com>
2019-06-11 11:43:40 -0500

    homepage js updates

--- js/index.js
+++ js/index.js
@@ -1,54 +1,50 @@
-// Steps
+let steps = document.querySelectorAll('.home-products__item');
 
-for(let i = 1; i <= 6; i++)
-{
-let tl1 = new TimelineMax({ onUpdate: onUpdate });
-      const controller1 = new ScrollMagic.Controller();
+for(let i = 0; i < steps.length; i+=1) {
+    let step = steps[i];
+    let number = step.querySelector('.home-products__item-img-step');
+    let logo = step.querySelector('.home-products__item-img-logo');
 
-tl1.to('.home-products__item-img-step--' + i, 1, { y: -275, opacity: .9 });
-tl1.to('.home-products__item-img-logo--' + i, 2, { y: -50 }, "-=1");
+    let tl1 = new TimelineMax();
+    const controller1 = new ScrollMagic.Controller();
 
-const scene1 = new ScrollMagic.Scene({
-    triggerElement: '.home-products__item--' + i,
-    triggerHook: .7, //between 0 an 1
-    duration: '100%'
-})
-    // .setPin('.image-group__container')
-    .setTween(tl1)
-    // .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
-    .addTo(controller1);
+    tl1.to(number, 1, { y: -275, opacity: .9 });
+    tl1.to(logo, 2, { y: -50 }, "-=1");
 
-}
+    const scene1 = new ScrollMagic.Scene({
+        triggerElement: step,
+        triggerHook: .7, //between 0 an 1
+        duration: '100%'
+    })
+        // .setPin('.image-group__container')
+        .setTween(tl1)
+        // .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
+        .addTo(controller1);
 
-function onUpdate() {
-    //tl.progress();
-    //console.log('bob');
 }
 
 // Designs
 
-for (let i = 1; i <= 3; i++) {
-    let tl2 = new TimelineMax({ onUpdate: onUpdate });
+let screens = document.querySelectorAll('.image-group__image');
+
+for(let i = 0; i < screens.length; i+=1) {
+    let screen = screens[i];
+    let tl2 = new TimelineMax();
     const controller2 = new ScrollMagic.Controller();
 
-    tl2.from('.image-group__image--' + i, 1, { y: 50, opacity: 0, ease: Linear.easeNone });
+    tl2.from(screen, 1, { y: 50, opacity: 0, ease: Linear.easeNone });
 
     const scene1 = new ScrollMagic.Scene({
-        triggerElement: '.image-group__image--' + i,
-        triggerHook: 1, //between 0 an 1
+        triggerElement: screen,
+        triggerHook: .5, //between 0 an 1
         duration: '35%'
     })
         // .setPin('.image-group__container')
         .setTween(tl2)
         // .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
         .addTo(controller2);
-
 }
 
-function onUpdate() {
-    //tl.progress();
-    //console.log('bob');
-}
 
 
 
