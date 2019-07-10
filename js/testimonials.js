957fd9f2aa82c5c6d4cfa44a329a80d071d0b981
Matthew Sparks <msparks@doctorlogic.com>
2019-07-10 14:40:35 -0500

    adding testimonials

---
+++ js/testimonials.js
@@ -0,0 +1,22 @@
+function resizeGridItem(item) {
+    grid = document.getElementsByClassName("testimonials__items")[0];
+    rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+    rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+    rowSpan = Math.ceil((item.querySelector('.testimonials__item-container').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+    item.style.gridRowEnd = "span " + rowSpan;
+}
+
+function resizeAllGridItems() {
+    allItems = document.getElementsByClassName("testimonials__item");
+    for (x = 0; x < allItems.length; x++) {
+        resizeGridItem(allItems[x]);
+    }
+}
+
+function resizeInstance(instance) {
+    item = instance.elements[0];
+    resizeGridItem(item);
+}
+
+window.onload = resizeAllGridItems();
+window.addEventListener("resize", resizeAllGridItems);
