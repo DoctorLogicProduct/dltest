d977cc01cd18250af1982a432493c19b24dc6a13
Matthew Sparks <msparks@doctorlogic.com>
2020-02-04 13:04:08 -0600

    redesign index changes

--- js/index.js
+++ js/index.js
@@ -1,49 +1,4 @@
-let steps = document.querySelectorAll('.home-products__item');
 
-for(let i = 0; i < steps.length; i+=1) {
-    let step = steps[i];
-    let number = step.querySelector('.home-products__item-img-step');
-    let logo = step.querySelector('.home-products__item-img-logo');
-
-    let tl1 = new TimelineMax();
-    const controller1 = new ScrollMagic.Controller();
-
-    tl1.to(number, 1, { y: -275, opacity: .9 });
-    tl1.to(logo, 2, { y: -50 }, "-=1");
-
-    const scene1 = new ScrollMagic.Scene({
-        triggerElement: step,
-        triggerHook: .7, //between 0 an 1
-        duration: '100%'
-    })
-        // .setPin('.image-group__container')
-        .setTween(tl1)
-        // .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
-        .addTo(controller1);
-
-}
-
-// Designs
-
-let screens = document.querySelectorAll('.image-group__image');
-
-for(let i = 0; i < screens.length; i+=1) {
-    let screen = screens[i];
-    let tl2 = new TimelineMax();
-    const controller2 = new ScrollMagic.Controller();
-
-    tl2.from(screen, 1, { y: 50, opacity: 0, ease: Linear.easeNone });
-
-    const scene1 = new ScrollMagic.Scene({
-        triggerElement: screen,
-        triggerHook: 1, //between 0 an 1
-        duration: '35%'
-    })
-        // .setPin('.image-group__container')
-        .setTween(tl2)
-        // .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
-        .addTo(controller2);
-}
 
 
 // Case Studies
@@ -91,23 +46,6 @@ const scene4 = new ScrollMagic.Scene({
 
 
 // // Growth Accelerators
-
-// tl5 = new TimelineMax();
-// const controller5 = new ScrollMagic.Controller();
-
-// tl5.from('.home-growth__icon', 1, { rotationY: 360 });
-
-
-// const scene5 = new ScrollMagic.Scene({
-//     triggerElement: '.home-growth',
-//     triggerHook: .8, //between 0 an 1
-//     duration: '50%'
-// })
-//     // .setPin('.image-group__container')
-//     .setTween(tl5)
-//     // .addIndicators({ name: "2 (duration: 300)", colorTrigger: "blue" })
-//     .addTo(controller5);
-
 
 if ($(window).width() >= 568) {
 tl5 = new TimelineMax({ onUpdate: onUpdate });
@@ -159,30 +97,6 @@ else {
     //console.log('bob');
 }
 }
-
-// Hero Confetti Settings
-
-// var confettiSettings = {
-//     target: 'hero-confetti',
-//     rotate: true,
-//     colors: [[11, 108, 181], [0, 184, 180], [237, 22, 121], [78, 101, 175], [102, 205, 238], [165, 80, 159]],
-//     clock: 30,
-//     props: [
-//         "circle",
-//         "square"
-//     ]
-// };
-// var confetti = new ConfettiGenerator(confettiSettings);
-// confetti.render();
-
-// // Hero Inc Logo
-
-// const tl6 = new TimelineMax();
-
-// tl6.to('.hero-1__image', 2, { rotationY: 360, repeatDelay: 5, repeat: -1, yoyo: true });
-
-// tl6.play();
-
 
 
 
